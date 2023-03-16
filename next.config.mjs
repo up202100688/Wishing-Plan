64ec5345d744a44f33410af569226f455f097442
Andreas Petersen (Guldberg) <andreasgdp@gmail.com>
2023-03-16 21:19:19 +0100

    Solve missing env issue when testing (#110)

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,19 @@
-import withPWA from 'next-pwa';
-
 // @ts-check
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
  * This is especially useful for Docker builds.
- * 
- * @example SKIP_ENV_VALIDATION=1 yarn build
  */
-!process.env.SKIP_ENV_VALIDATION && (await import('./src/env/server.mjs'));
+!process.env.SKIP_ENV_VALIDATION && (await import('./src/env.mjs'));
 
 /** @type {import("next").NextConfig} */
 const config = {
 	reactStrictMode: true,
-	swcMinify: true,
+	/* If trying out the experimental appDir, comment the i18n config out
+	 * @see https://github.com/vercel/next.js/issues/41980 */
 	i18n: {
 		locales: ['en'],
 		defaultLocale: 'en',
 	},
+	output: 'standalone',
 };
-
-const nextConfig = withPWA({
-	dest: 'public',
-})(config);
-
-export default nextConfig;
+export default config;
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -5,9 +5,9 @@ import GoogleProvider from 'next-auth/providers/google';
 // Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 
-import { env } from '@env/server.mjs';
 import { prisma } from '@server/db/client';
 import { signInChecks } from '@server/trpc/utils/auth/signInUtils';
+import { env } from 'src/env.mjs';
 
 const secret = process.env.NEXTAUTH_SECRET;
 
